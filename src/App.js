3b5ce936fa6c2f277c662fe913cec44fe6700bf4
Xavier <xavierroy812@gmail.com>
2025-02-05 23:04:18 +0530

    Added backend folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-
-import About from './about/About';
-import './App.css';
-import UserHomepage from './homepage/UserHomepage';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import UserHomepage from "./homepage/UserHomepage";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     <About/>
-    </div>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<UserHomepage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
