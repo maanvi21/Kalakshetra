3172583f57766c95633a916918fccf1d0620f37c
Maanvi Shadakshari <maanvishadakshari21@gmail.com>
2025-02-06 19:59:21 +0530

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+// import { AuthProvider } from "./context/AuthContext";
+import About from './about/About.js'
+import Browse from './browse/Browse.js'
 import UserHomepage from "./homepage/UserHomepage";
 import Login from "./auth/Login";
+import Wishlist from './wishlist/Wishlist.js'
+import Contact from './contact/Contact.js'
 import Register from "./auth/Register";
 import "./App.css";
+import Context from "./context/Context";
 
 function App() {
   return (
-    <AuthProvider>
+    <Context>
       <Router>
         <div className="App">
           <Routes>
-            <Route path="/" element={<UserHomepage />} />
+          <Route path='/' element={[<UserHomepage/>]}/>
+
+<Route path='/userbrowse' element={[<Browse/>]}/>
+<Route path='/wishlist' element={[<Wishlist/>]}/>
+<Route path='/about' element={[<About/>]}/>
+<Route path='/contact' element={[<Contact/>]}/>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
           </Routes>
         </div>
       </Router>
-    </AuthProvider>
+    </Context>
   );
 }
 
