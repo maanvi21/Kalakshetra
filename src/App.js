ac4d5046b088e0012d83c35a0ab14f821541e816
Maanvi Shadakshari <maanvishadakshari21@gmail.com>
2025-02-07 22:46:56 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from './about/About.js'
 import Browse from './browse/Browse.js'
 import UserHomepage from "./homepage/UserHomepage";
-import Login from "./auth/Login";
 import Wishlist from './wishlist/Wishlist.js'
 import Contact from './contact/Contact.js'
-import Register from "./auth/Register";
+
 import "./App.css";
 import WishlistContext from "./context/WishlistContext.js";
 import { AuthProvider } from "./context/AuthContext.js";
+import AuthForm from "./auth/AuthForm.jsx";
 
 function App() {
   return (
@@ -24,8 +24,8 @@ function App() {
 <Route path='/wishlist' element={[<Wishlist/>]}/>
 <Route path='/about' element={[<About/>]}/>
 <Route path='/contact' element={[<Contact/>]}/>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<AuthForm/>} />
+           
             <Route path="/browse" element={<Browse />} />
           </Routes>
         </div>
