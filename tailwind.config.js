5d4908cc24bb1d551f74d2b0530e481c8fb30d12
Xavier <xavierroy812@gmail.com>
2025-02-08 00:40:13 +0530

    working auth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-bg': '#D3BFB1',
+      },
+    },
   },
   plugins: [],
-};
+}
