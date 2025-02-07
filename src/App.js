5cb3e3437959ee5fc9121bdf4942d8995cd5b803
Xavier <xavierroy812@gmail.com>
2025-02-07 23:50:25 +0530

    With changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { AuthProvider } from "./context/AuthContext";
-import About from './about/About.js'
-import Browse from './browse/Browse.js'
+import About from "./about/About.js";
+import Browse from "./browse/Browse.js";
 import UserHomepage from "./homepage/UserHomepage";
-import Login from "./auth/Login";
-import Wishlist from './wishlist/Wishlist.js'
-import Contact from './contact/Contact.js'
-import Register from "./auth/Register";
+import AuthForm from "./auth/AuthForm.jsx"; // Renamed import for clarity
+import Wishlist from "./wishlist/Wishlist.js";
+import Contact from "./contact/Contact.js";
 import "./App.css";
 import Context from "./context/Context";
 
@@ -16,14 +14,13 @@ function App() {
       <Router>
         <div className="App">
           <Routes>
-          <Route path='/' element={[<UserHomepage/>]}/>
-
-<Route path='/userbrowse' element={[<Browse/>]}/>
-<Route path='/wishlist' element={[<Wishlist/>]}/>
-<Route path='/about' element={[<About/>]}/>
-<Route path='/contact' element={[<Contact/>]}/>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<UserHomepage />} />
+            <Route path="/userbrowse" element={<Browse />} />
+            <Route path="/wishlist" element={<Wishlist />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<AuthForm />} />
+            <Route path="/register" element={<AuthForm />} />
           </Routes>
         </div>
       </Router>
