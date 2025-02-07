3fe30773ece416ead7ab2df264d27b081062ee67
Maanvi Shadakshari <maanvishadakshari21@gmail.com>
2025-02-07 13:23:21 +0530

    Fixed the uder homepage

--- src/homepage/UserHomepage.js
+++ src/homepage/UserHomepage.js
@@ -1,29 +1,12 @@
 import React from 'react';
 import './UserHomepage.css';
-
+import carouselItems from '../data/CarouselData.js'
+import categories from '../data/CategoriesData.js'
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import HomepagCarousel from './HomepagCarousel';
-const carouselItems = [
-  {
-    image: "assets/homeimg2.png",
-    alt: "Art 1",
-    title: "Beautiful Art",
-    description: "A masterpiece of creativity.",
-  },
-  {
-    image: "assets/homeimg3.png",
-    alt: "Art 2",
-    title: "Cultural Heritage",
-    description: "Preserving traditions through art.",
-  },
-  {
-    image: "assets/homeimg4.png",
-    alt: "Art 3",
-    title: "Modern Expression",
-    description: "Blending past and future.",
-  },
-];
+import HomepagCarousel from './HomepagCarousel.js';
+import CategoriesContainer from '../components/CategoriesContainer';
+
 const UserHomepage = () => {
   return (
     <div className="user-homepage">
@@ -33,39 +16,19 @@ const UserHomepage = () => {
         <HomepagCarousel items={carouselItems}/>
 
         <section className="offerings">
+          {/* categories */}
+          
           <h2>What we offer</h2>
+          
           <div className="offerings-grid">
-            <div>
-              <h3>Sarees</h3>
-              <p>Handloom hand-painted sarees</p>
-              <button>Browse</button>
-            </div>
-            <div>
-              <h3>Sarees</h3>
-              <p>Handloom hand-painted sarees</p>
-              <button>Browse</button>
-            </div>
-            <div>
-              <h3>Sarees</h3>
-              <p>Handloom hand-painted sarees</p>
-              <button>Browse</button>
-            </div>
-            <div>
-              <h3>Dresses</h3>
-              <p>Handloom hand-painted dresses</p>
-              <button>Browse</button>
-            </div>
-            <div>
-              <h3>Dresses</h3>
-              <p>Handloom hand-painted dresses</p>
-              <button>Browse</button>
-            </div>
+          <CategoriesContainer items={categories}/>
           </div>
         </section>
 
         <section className="branches">
           <h2>Locate our branches</h2>
           <div className="branch-grid">
+            
             <div>
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.185780985396!2d80.22822871538961!3d13.054792390790354!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52667bad989ed7%3A0x6d8be7ba2aeb6fa3!2sKalakshetra%20Foundation!5e0!3m2!1sen!2sin!4v1675372741935!5m2!1sen!2sin"
