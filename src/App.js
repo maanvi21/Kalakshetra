555a56d31eb311d2f286ead5be43a3bece788448
Maanvi Shadakshari <maanvishadakshari21@gmail.com>
2025-02-08 18:35:46 +0530

    Fixing without auth and cart

--- src/App.js
+++ src/App.js
@@ -2,29 +2,43 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./about/About.js";
 import Browse from "./browse/Browse.js";
 import UserHomepage from "./homepage/UserHomepage";
-import AuthForm from "./auth/AuthForm.jsx"; // Renamed import for clarity
-import Wishlist from "./wishlist/Wishlist.js";
-import Contact from "./contact/Contact.js";
+import Wishlist from './wishlist/Wishlist.js'
+import Contact from './contact/Contact.js'
+
+
+
+
 import "./App.css";
-import Context from "./context/Context";
+import WishlistContext from "./context/WishlistContext.js";
+import { AuthProvider } from "./context/AuthContext.js";
+
 
 function App() {
   return (
-    <Context>
+    <AuthProvider>
+    <WishlistContext>
       <Router>
         <div className="App">
           <Routes>
+          <Route path='/' element={[<UserHomepage/>]}/>
+
+<Route path='/userbrowse' element={[<Browse/>]}/>
+<Route path='/wishlist' element={[<Wishlist/>]}/>
+<Route path='/about' element={[<About/>]}/>
+<Route path='/contact' element={[<Contact/>]}/>
+           
+            <Route path="/browse" element={<Browse />} />
             <Route path="/" element={<UserHomepage />} />
             <Route path="/userbrowse" element={<Browse />} />
             <Route path="/wishlist" element={<Wishlist />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/login" element={<AuthForm />} />
-            <Route path="/register" element={<AuthForm />} />
+
           </Routes>
         </div>
       </Router>
-    </Context>
+    </WishlistContext>
+    </AuthProvider>
   );
 }
 
