f1405c7b2247716108e8bd619796deb093ce16b8
Xavier <xavierroy812@gmail.com>
2025-05-02 18:19:43 +0530

    orders

--- src/App.js
+++ src/App.js
@@ -2,66 +2,60 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./about/About.js";
 import Browse from "./browse/Browse.js";
 import UserHomepage from "./homepage/UserHomepage";
-import Wishlist from './wishlist/Wishlist.js'
-import Contact from './contact/Contact.js'
+import Wishlist from './wishlist/Wishlist.js';
+import Contact from './contact/Contact.js';
 import AdminBrowse from "./browse/AdminBrowse.js";
 import AuthForm from "./auth/AuthForm.jsx";
 import MenProducts from "./browse/MenProducts.js";
 import WomenProducts from "./browse/WomenProducts.js";
 import BagsProducts from "./browse/BagsProducts.js";
+import AccessoriesProducts from "./browse/AccessoriesProducts.js";
+import AdminManagement from "./browse/AdminManagement.js";
+import AuthHandler from "./auth/AuthHandler.js";
+import Cart from "./cart/Cart.js";
+
 import "./App.css";
 import { AuthProvider } from "./context/AuthContext.js";
 import CartProvider from './context/CartContext.js';
-import Cart from "./cart/Cart.js";
 import WishlistProvider from "./context/WishlistContext.js";
-import AccessoriesProducts from "./browse/AccessoriesProducts.js";
-import AuthHandler from "./auth/AuthHandler.js";
-import AdminManagement from "./browse/AdminManagement.js";
 
-
+// ✅ Component to redirect to static orders.html
+function NavigateToOrders() {
+  window.location.href = "/orders.html";
+  return null;
+}
 
 function App() {
   return (
-
     <AuthProvider>
       <CartProvider>
-    <WishlistProvider>
-      <Router>
-        <AuthHandler/>
-        <div className="App">
-          <Routes>
-          <Route path='/' element={[<UserHomepage/>]}/>
-          <Route path='/menProducts' element={[<MenProducts/>]}/>
-          <Route path='/womenProducts' element={[<WomenProducts/>]}/>
-          <Route path='/bagsProducts' element={[<BagsProducts/>]}/>
-          <Route path='/accessoriesProducts' element={[<AccessoriesProducts/>]}/>
-<Route path='/userbrowse' element={[<Browse/>]}/>
-<Route path='/wishlist' element={[<Wishlist
-/>]}/>
-<Route path='/about' element={[<About/>]}/>
-<Route path='/contact' element={[<Contact/>]}/>
-<Route path='/adminmanagement' element={[<AdminManagement/>]}/>
-           
-            <Route path="/browse" element={<Browse />} />
-            <Route path="/" element={<UserHomepage />} />
-            <Route path="/userbrowse" element={<Browse />} />
-            <Route path="/wishlist" element={<Wishlist />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/login" element={<AuthForm />} />
-            <Route path="/register" element={<AuthForm />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/adminbrowse" element={<AdminBrowse />} />
-            {/* <Route path='/locate' element={<LocateUs/>} /> */}
-
-          </Routes>
-        </div>
-      </Router>
-      </WishlistProvider>
+        <WishlistProvider>
+          <Router>
+            <AuthHandler />
+            <div className="App">
+              <Routes>
+                <Route path="/" element={<UserHomepage />} />
+                <Route path="/menProducts" element={<MenProducts />} />
+                <Route path="/womenProducts" element={<WomenProducts />} />
+                <Route path="/bagsProducts" element={<BagsProducts />} />
+                <Route path="/accessoriesProducts" element={<AccessoriesProducts />} />
+                <Route path="/userbrowse" element={<Browse />} />
+                <Route path="/browse" element={<Browse />} />
+                <Route path="/wishlist" element={<Wishlist />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/login" element={<AuthForm />} />
+                <Route path="/register" element={<AuthForm />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/orders" element={<NavigateToOrders />} /> {/* ✅ Added route */}
+                <Route path="/adminbrowse" element={<AdminBrowse />} />
+                <Route path="/adminmanagement" element={<AdminManagement />} />
+              </Routes>
+            </div>
+          </Router>
+        </WishlistProvider>
       </CartProvider>
-    
     </AuthProvider>
-
   );
 }
 
