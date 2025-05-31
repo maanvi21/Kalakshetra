944d78d00907397169afca5c1a948c121cc04863
kodeandco <hello.kodeandco@gmail.com>
2025-05-31 14:46:12 +0530

    deloyment

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://kalakshetra3-zyhn.vercel.app/api/auth";
+const API_URL = "https://kalakshetra3-zyhn.vercel.app/auth";
 
 export const register = async (userData) => {
     return axios.post(`${API_URL}/register`, userData);
